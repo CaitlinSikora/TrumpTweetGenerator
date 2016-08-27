e4f0a90f9a1b5ac2913d900f217f1aad1a8a1bb8
CaitlinSikora <caitlin.sikora@gmail.com>
2016-08-27 18:19:04 -0400

    removed gitignore files

--- config.py
+++
@@ -1,7 +0,0 @@
-CSRF_ENABLED = True
-SECRET_KEY = 'this-is-a-secret'
-
-GOOGLE_KEYS = {
-	'search_engine_id':'015037131198447628591:7g7ithuxakq',
-	'apis':['AIzaSyBTg-ed_rVXHl4khSn8EXB3wC-F6-IP6tM','AIzaSyBy8oibtBr1kJu4UEL6uVOXkh-IHS9AHzs','AIzaSyAFjKNFRy13MnMvYUYA36kzTfPdGTrPEEk']
-}
